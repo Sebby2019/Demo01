dbfb96c9d7e34c630524856445d0871b1636a498
狼影啸月 <1535595929@qq.com>
2019-07-20 00:49:08 +0800

    知识点3

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,27 @@
 Page({
-
+  data: {
+    name: 'Coderwhy',
+    age: 18,
+    students: [
+      { id: 110, name: 'kobe', age: 30 },
+      { id: 111, name: 'james', age: 28 },
+      { id: 112, name: 'curry', age: 32 },
+      { id: 110, name: 'why', age: 18 }
+    ],
+    counter: 0
+  },
+  handleAddClick() {
+    // 1.错误做法：界面是不会刷新的
+    // this.data.counter += 1
+    // console.log(this.data.counter)
+    // 2.this.setData()
+    this.setData({
+      counter: this.data.counter + 1
+    })
+  },
+  handleSubClick() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
+  }
 })
